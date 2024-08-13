2314447a3bcee08edeadaf0c5a518f5c7434e637
Focus0Wizard <150725448+Focus0Wizard@users.noreply.github.com>
2024-08-13 10:16:04 -0400

    add rest, mult and div opeartions

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,42 @@
 import sumar from "./sumador";
+import restar from "./restador";
+import multi from "./multiplicador";
+import dividir from "./divisor";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const buttons = {
+  sumar: document.querySelector("#sumar-button"),
+  restar: document.querySelector("#rest-button"),
+  multiplicar: document.querySelector("#mult-button"),
+  dividir: document.querySelector("#div-button")
+};
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+Object.keys(buttons).forEach((key) => {
+  buttons[key].addEventListener("click", (event) => {
+    event.preventDefault();
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+    const firstNumber = Number.parseInt(first.value);
+    const secondNumber = Number.parseInt(second.value);
+    let resultado;
+
+    switch (key) {
+      case "sumar":
+        resultado = sumar(firstNumber, secondNumber);
+        break;
+      case "restar":
+        resultado = restar(firstNumber, secondNumber);
+        break;
+      case "multiplicar":
+        resultado = multi(firstNumber, secondNumber);
+        break;
+      case "dividir":
+        resultado = dividir(firstNumber, secondNumber);
+        break;
+    }
+
+    div.innerHTML = "<p>" + resultado + "</p>";
+  });
 });
