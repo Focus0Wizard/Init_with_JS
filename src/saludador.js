8234c431aadf6e1137cbf75a4b9ff342ca5c930d
Focus0Wizard <150725448+Focus0Wizard@users.noreply.github.com>
2024-08-18 19:11:21 -0400

    Implementation of a selector to enter the gender and custom more the "hello"

--- src/saludador.js
+++ src/saludador.js
@@ -1,11 +1,22 @@
 const form = document.querySelector("#form-saludator");
 const name = document.querySelector("#User-name");
+const gender = document.querySelector("#gender");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const Hi  = "Hola"
+    let Hi  = "Hola"
     const nombre = name.value
+    const genero = gender.value
+
+    if(genero === "male"){
+        Hi = Hi + " Sr. "
+    }else if(genero === "female"){
+        Hi = Hi + " Sra. "
+    }else{
+        Hi = Hi + " "
+    }
+
     const Saludo = Hi + " " + nombre
     div.innerHTML = "<p>" + Saludo  + "</p>";
 });
