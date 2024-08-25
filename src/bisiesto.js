7de03abf7c98fd51bd2e8e3636cec79d9794d7dd
Focus0Wizard <150725448+Focus0Wizard@users.noreply.github.com>
2024-08-25 17:50:28 -0400

    add 3 rules for "LeapYear" and test for all adds

--- src/bisiesto.js
+++ src/bisiesto.js
@@ -3,6 +3,12 @@ function isLeapYear(year) {
     
     if(year % 400 === 0){
         resp = "Yes"
+    }else if(year % 100 === 0 && year % 400 != 0){
+        resp = "No"
+    }else if(year % 4 === 0 && year % 100 != 0){
+        resp = "Yes"
+    }else if(year % 4 != 0){
+        resp = "No"
     }
 
     return resp
--- src/bisiesto.spec.js
+++ src/bisiesto.spec.js
@@ -1,8 +1,20 @@
 import isLeapYear from "./bisiesto.js";
 
 describe("FizzBuzz", () => {
-    it("El año ingresado no sigue ningua regla", () => {
+    it("El año ingresado no sigue la regla de: Es bisiesto :Divisible entre 400:", () => {
         expect(isLeapYear(2000)).toEqual("Yes");
     });
     
+    it("El año ingresado no sigue la regla de: No es bisiesto :Divisible entre 100 pero no entre 400:", () => {
+        expect(isLeapYear(1800)).toEqual("No");
+    });
+
+    it("El año ingresado no sigue la regla de: Es bisiesto :Divisible entre 4 pero no entre 100:", () => {
+        expect(isLeapYear(2016)).toEqual("Yes");
+    });
+
+    it("El año ingresado no sigue la regla de: No es bisiesto :No divisible entre 4:", () => {
+        expect(isLeapYear(2019)).toEqual("No");
+    });
+
 });
